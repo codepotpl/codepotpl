d74fe4408d84885f7132915015b6b35f8fb41e0f
Tomasz Netczuk <netczuk.tomasz@gmail.com>
2015-04-19 13:09:33 +0200

    Organizers and tutors data in MD and YAML.

--- index.js
+++ index.js
@@ -1,11 +1,20 @@
 var express = require('express');
 var compression = require('compression');
+var YAML = require('yamljs');
+var markdown = require('markdown');
 var app = express();
 
 app.use(compression());
 app.set('view engine', 'jade');
 app.set('views', __dirname + '/src/templates');
 app.use('/public', express.static(__dirname + '/public'));
+
+var data = {
+    organizers : YAML.load('src/data/organizers.yml'),
+    tutors : YAML.load('src/data/tutors.yml')
+};
+
+console.log(data);
 
 app.get('/', function (req, res) {
     res.render('index');
