508294458f23cb8743ee4f9a06d17897b2635e6b
Tomasz Netczuk <netczuk.tomasz@gmail.com>
2015-05-05 23:44:28 +0200

    Added social images.

--- README.md
+++ README.md
@@ -7,13 +7,13 @@ Codepot website
 2. install bower dependencies: `bower install`,
 3. build grunt: `grunt`,
 4. start grunt watcher: `grunt watch`,
-5. start the app: `node index.js`.
+5. start the app: `CDPT_HOST="http://localhost:8080/" node index.js`.
 
 # Staging/release builds
 To build for staging/release environment just run docker:
 
 - `docker build --tag codepotpl .` to build image,
-- `docker run -it -p 8080:8080 codepotpl` to run container with exposed port 8080.
+- `docker run -it -p 8080:8080 -e CDPT_HOST="<HOST_HERE>" codepotpl` to run container with exposed port 8080.
 
 ## Known issues:
 
--- index.js
+++ index.js
@@ -1,3 +1,19 @@
+var REQUIRED_ENV_VARIABLES = [
+    'CDPT_HOST'
+];
+
+var MISSING_ENV_VARIABLES = [];
+for (var i = 0; i < REQUIRED_ENV_VARIABLES.length; i++) {
+    var variable = REQUIRED_ENV_VARIABLES[i];
+    if (!process.env[variable]) {
+        MISSING_ENV_VARIABLES.push(variable);
+    }
+}
+if (MISSING_ENV_VARIABLES.length > 0) {
+    var message = 'Missing environment variables: ' + MISSING_ENV_VARIABLES.join(', ');
+    throw new Error(message);
+}
+
 var express = require('express');
 var compression = require('compression');
 var YAML = require('yamljs');
@@ -11,7 +27,7 @@ app.use('/public', express.static(__dirname + '/public'));
 
 var data = {
     organizers: YAML.load('src/data/organizers.yml'),
-    tutors: YAML.load('src/data/tutors.yml').filter(function(tutor){
+    tutors: YAML.load('src/data/tutors.yml').filter(function (tutor) {
         return tutor.publish;
     }),
     sponsors: YAML.load('src/data/sponsors.yml')
@@ -27,7 +43,8 @@ var metaTagsData = {
     type: "website",
     author: "http://codepot.pl/",
     publisher: "http://codepot.pl/",
-    twitterSite: "@codepot_pl"
+    twitterSite: "@codepot_pl",
+    host: process.env['CDPT_HOST']
 };
 
 app.get('/', function (req, res) {
