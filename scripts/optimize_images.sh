e43a2ec62fcb2e0b164c8f62cd905b0489667563
Marcin Jasion <marcin.jasion@gmail.com>
2015-05-24 14:17:49 +0200

    refactored Dockerfile and script for image optimize

---
+++ scripts/optimize_images.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+# https://tinypng.com/developers
+
+# SELECT ONE
+API_KEY='vyMxyN-TZO5L72m8b_RQpkxYejUqpdO4'
+#API_KEY='fbZsqLrBzamzKBJwRzCLQv2zRgV2hoQv'
+#API_KEY='BSJbnwLRMAFavB2ga14kxP__qjfyv9s1'
+
+function optimize {
+  for image in `find . -type f -name "*.$1"`; do
+    echo "Opimizing $image"
+    JSON=`curl -q --user api:"$API_KEY" --data-binary @$image https://api.tinify.com/shrink`
+    URL=${JSON/*url\":\"/};
+    URL=${URL/\"*/};
+    curl -q $URL > $image
+  done
+}
+
+optimize 'jpeg'
+optimize 'jpg'
+optimize 'png'
