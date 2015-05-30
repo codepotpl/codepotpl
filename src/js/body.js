f194859555888f83ea66ef602ce641d92525fa42
Tomasz Netczuk <netczuk.tomasz@gmail.com>
2015-05-30 12:58:54 +0200

    Made google maps loading async.

--- src/js/body.js
+++ src/js/body.js
@@ -11,27 +11,22 @@ function initialize() {
         mapTypeControl: false,
         scaleControl: false,
         draggable: false,
-        center: new google.maps.LatLng(52.248421,21.066009),
+        center: new google.maps.LatLng(52.248421, 21.066009),
         zoom: 17,
         mapTypeId: google.maps.MapTypeId.ROADMAP
-    }
+    };
     map = new google.maps.Map(mapCanvas, mapOptions);
     var marker = new google.maps.Marker({
         position: new google.maps.LatLng(52.248421, 21.066009),
         map: map,
-        title:"CODEPOT will be here!"
+        title: "CODEPOT will be here!"
     });
 }
 
-google.maps.event.addDomListener(window, 'load', initialize);
-document.getElementById("show_map").onclick = function(){
-    document.getElementById("map").style.display = 'inline';
-    document.getElementById("show_map").style.display = 'none';
-
-
-
-    currentCenter = map.getCenter();
-    google.maps.event.trigger(map, 'resize');
-    map.setCenter(currentCenter);
-}
+document.getElementById("show_map").onclick = function () {
+    yepnope.injectJs('https://maps.googleapis.com/maps/api/js?callback=initialize', function () {
+        document.getElementById("map").style.display = 'inline';
+        document.getElementById("show_map").style.display = 'none';
+    });
+};
 //END GOOGLE MAP
