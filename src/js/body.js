e43b54eb9da2452701c020c41759678fa9604bc6
Andrzej Chmielowski <akhmelovskyi@bluesoft.net.pl>
2015-07-05 17:50:43 +0200

    developing

--- src/js/body.js
+++ src/js/body.js
@@ -115,3 +115,36 @@ function hasScrolled() {
     lastScrollTop = st;
 }
 /**END HIDE/SHOW NAV BAR ON SCROLL*/
+
+/**CAROUSEL for partners and media*/
+$(document).ready(function() {
+    $('.your-class').slick({
+        dots: true,
+        infinite: false,
+        speed: 500,
+        slidesToShow: 4,
+        slidesToScroll: 4,
+        responsive: [{
+            breakpoint: 1024,
+            settings: {
+                slidesToShow: 3,
+                slidesToScroll: 3,
+                infinite: true,
+                dots: true
+            }
+        }, {
+            breakpoint: 600,
+            settings: {
+                slidesToShow: 2,
+                slidesToScroll: 2
+            }
+        }, {
+            breakpoint: 480,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1
+            }
+        }]
+    });
+});
+/**END CAROUSEL for partners and media*/
